f350b294aa2525606515104775a62c45da318f11
CyberVitexus <info@vitexsoftware.cz>
2023-12-06 15:37:52 +0100

    Expample Update

--- examples/getaccounts.php
+++ examples/getaccounts.php
@@ -5,12 +5,9 @@ namespace VitexSoftware\Raiffeisenbank;
 require_once('../vendor/autoload.php');
 
 \Ease\Shared::init(['CERT_FILE','CERT_PASS','XIBMCLIENTID'],__DIR__.'/example.env');
-
-$apiInstance = new PremiumAPI\GetAccountsApi(new ApiClient());
-$x_ibm_client_id = \Ease\Shared::cfg('XIBMCLIENTID'); // string | ClientID obtained from Developer Portal - when you registered your app with us.
+        
+$apiInstance = new PremiumAPI\GetAccountsApi();
 $x_request_id = time(); // string | Unique request id provided by consumer application for reference and auditing.
-$accept_language = 'cs'; // string | The Accept-Language request HTTP header is used to determine document  language. Supported languages are `cs` and `en`.
-$psu_ip_address = ApiClient::getPublicIP(); // string | IP address of a client - the end IP address of the client application (no server) in IPv4 or IPv6 format.
 //If the bank client (your user) uses a browser by which he accesses your server app, we need to know the IP address of his browser.
 //Always provide the closest IP address to the real end-user possible.
 
